6f5e23dde150b5efdd42a6d7f0f20f1944bb6e89
annimarie01 <anni.thekindfairy@gmail.com>
2026-04-21 12:31:46 -0600

    movie-info

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //API Key: https://www.omdbapi.com/?i=tt3896198&apikey=cfa91572
+
+async function getMovieData() {
+    let movie = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=cfa91572");
+    let movieData = await movie.json();
+    console.log(movieData);
+}
+
+getMovieData();
