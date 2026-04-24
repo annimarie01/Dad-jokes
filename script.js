c8db95081146d3b4408085564d034577112e01ce
annimarie01 <anni.thekindfairy@gmail.com>
2026-04-23 21:15:33 -0600

    api

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //API Key: https://www.omdbapi.com/?i=tt3896198&apikey=cfa91572
 
 async function getMovieData() {
-    let movie = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=cfa91572");
-    let movieData = await movie.json();
-    console.log(movieData);
+    const info = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=cfa91572");
+    const movie = await info.json();
+    
+    const showData = document.querySelector(".movie");
+    showData.innerHTML = movieHTML(movie);
 }
 
 getMovieData();
+
+function movieHTML(movie) {
+    return `<figure class="movie__poster--wrapper">
+                    <img src="${movie.Poster}" alt="${movie.Title}" class="movie__poster">
+                </figure>
+                <div class="movie__info">
+                    <h2 class="movie__title">${movie.Title}</h2>
+                    <div class="movie__rated">${movie.Rated}</div>
+                    <div class="movie__runtime">${movie.Runtime}</div>
+                    <div class="movie__genre">${movie.Genre}</div>
+                    <div class="movie__date">Released: ${movie.Released}</div>
+                    <p class="movie__plot">${movie.Plot}</p>
+            </div>`
+}
